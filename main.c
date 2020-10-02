d73c0a00aa1a374270543f8e23b340f1b47b8be0
Ethan <efitterer08@gmail.com>
2020-10-02 18:20:44 +0000

    first commit

---
+++ main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+    int weaponDMG;
+    int strength;
+    int sharpnessLevel;
+    if(sharpnessLevel > 6) {
+        printf("\nInvalid Sharpness Level. Maximum is 6. Please try again.\n");
+        return 0;
+    }
+    int smiteLevel;
+    int baneOfArthropodsLevel;
+    int giantKillerLevel;
+    int enderSlayerLevel;
+    int dragonHunterLevel;
+    int executeLevel;
+    int cubismLevel;
+    int firstStrikeLevel;
+    int impalingLevel;
+    int combatLevel;
+    int finalDamage;
+    printf("\nEnter your Sword's Sharpness Level:\n");\
+    scanf("%i", &sharpnessLevel);
+    return 0;
+}
